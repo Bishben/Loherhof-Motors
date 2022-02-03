757d5689dd8812bc409c9d29b8b1a7ba48229051
imopingames <imopingames@gmail.com>
2022-02-03 22:16:52 +0100

    -carsection.php is now available to add cars.PLEASE SEE TO IT.^_^

--- php/carsection.php
+++ php/carsection.php
@@ -23,15 +23,16 @@
     <li class="breadcrumb-item" aria-current="page">>&nbsp;Add Car</li>
   </ol>
 </nav>
+<form method="post">
 <div class="whole_container">
 <div class="cont">Add Car</div>
-  <div class="name">Name: <input type="text"></div>
-  <div class="img1">Image One: <input type="text"></div>
-  <div class="img2">Image Two: <input type="text"></div>
+  <div class="name">Name: <input type="text" name="n1"></div>
+  <div class="img1">Image One: <input type="text" name="n2"></div>
+  <div class="img2">Image Two: <input type="text" name="n3"></div>
 
   <div class="make">Make:
 
-  <select>
+  <select name="n4">
     <option selected="true" disabled="disabled">Select Make</option>
     <option>Audi</option>
     <option>Aston Martin</option>
@@ -49,25 +50,25 @@
 
 
   </div>
-  <div class="model">Model: <input type="text"></div>
+  <div class="model">Model: <input type="text" name="n5"></div>
 
 
   <div class="condition">Condition:
 
-  <select>
+  <select name="n6">
     <option selected="true" disabled="disabled">Select Condition</option>
     <option>New</option>
     <option>Old</option>
   </select>
   </div>
 
-  <div class="price">Price: <input type="text"></div>
-  <div class="kilo">Kilometers: <input type="text"></div>
+  <div class="price">Price: <input type="text" name="n7"></div>
+  <div class="kilo">Kilometers: <input type="text" name="n8"></div>
 
 
   <div class="color">Color:
 
-    <select>
+    <select name="n9">
       <option selected="true" disabled="disabled">Select Color</option>
       <option>Black</option>
       <option>White</option>
@@ -82,7 +83,7 @@
 
   <div class="feature">Featured:
 
-    <select>
+    <select name="n10">
       <option selected="true" disabled="disabled">Select</option>
       <option>Yes</option>
       <option>No</option>
@@ -96,6 +97,41 @@
 
 
 </div>
+</form>
+<?php
 
+  $servername = "localhost";
+  $username = "root";
+  $password = "2912";
+  $dbname = "loherhofmotors";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  }
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $nm =  $_REQUEST['n1'];
+      $im1 = $_REQUEST['n2'];
+      $im2 =  $_REQUEST['n3'];
+      $mk =  $_REQUEST['n4'];
+      $md = $_REQUEST['n5'];
+      $con =  $_REQUEST['n6'];
+      $pr =  $_REQUEST['n7'];
+      $km = $_REQUEST['n8'];
+      $cl =  $_REQUEST['n9'];
+      $ft =  $_REQUEST['n10'];
+
+
+      $sql = "insert into cars(Name, ImgOne, ImgTwo, Make, Model, Cond, Color, Price, Kilometers, Featured) values ('$nm', '$im1', '$im2', '$mk','$md','$con','$pr','$km','$cl','$ft',)";
+      if($conn->query($sql)==TRUE){
+        echo "New records created successfully";
+      }else {
+        echo "Error:".$sql."<br>".$conn->error;
+      }
+      }
+
+
+ ?>
 </body>
 </html>
