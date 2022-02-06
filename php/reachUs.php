4696a9180b3edebdad1c3cdd24db8fc841ad783c
Bishben <bishben10@gmail.com>
2022-02-06 15:07:57 +0100

    Update: -centered panel in reach_us page

--- php/reachUs.php
+++ php/reachUs.php
@@ -103,7 +103,7 @@
         <br>
         <h5>Loherhof Automarket Showroom:03</h5>
         <div class="map-responsive">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2512.9395191839494!2d6.143780797711363!3d50.96182245577264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0a43939973327%3A0x2ac29737bf1d5efc!2sLoherhof%20Geilenkirchen%20-%20Restaurant.%20Fitness.%20Golf.%20Tennis.%20Badminton%20and%20Events.!5e0!3m2!1sen!2sde!4v1643709784323!5m2!1sen!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2512.9395191839494!2d6.143780797711363!3d50.96182245577264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0a43939973327%3A0x2ac29737bf1d5efc!2sLoherhof%20Geilenkirchen%20-%20Restaurant.%20Fitness.%20Golf.%20Tennis.%20Badminton%20and%20Events.!5e0!3m2!1sen!2sde!4v1643709784323!5m2!1sen!2sde" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
         <br>
       </div>
